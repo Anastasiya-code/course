a604dea760f694004e560ff3ddf86c0c39023e8c
Anastasiya <nastyushad@gmail.com>
2021-07-03 20:23:43 +0300

    Итоговая задача 4

---
+++ Итоговая_задача_4.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Итоговая_задача_4 {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Отгадайте загадку: Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        System.out.println("Введите ответ:");
+        for (int i = 1; i <= 3; i++) {
+            String str1 = scanner.nextLine();
+
+            switch (str1) {
+                case "Заархивированный вирус":
+                    System.out.println("Правильно!");
+                    return;
+                case "Подсказка":
+                    if (i == 1) {
+                        System.out.println("Блокирует работу твоего устройства");
+                        String str2 = scanner.nextLine();
+                        switch (str2) {
+                            case "Заархивированный вирус":
+                                System.out.println("Правильно!");
+                                return;
+                            default:
+                                System.out.println("Обидно, приходи в другой раз");
+                                return;
+                        }
+                    }
+                    if (i > 1) {
+                        System.out.println("Подсказка уже недоступна");
+                        continue;
+                    }
+                default:
+                    if (i<=2)
+                        System.out.println("Подумай еще!");
+                    else
+                        System.out.println("Обидно, приходи в другой раз");
+
+            }
+        }
+    }
+}
+
+
+
+
